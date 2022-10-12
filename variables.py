c011933d9ee4fac6ccaeee25a01d6b41b00a6571
Kunter <kunter.grupmann@voco.ee>
2022-10-12 13:46:55 +0300

    Ülesanne: muutuja

---
+++ variables.py
@@ -0,0 +1,9 @@
+word = "spider"
+thousand = "1000"
+ten = 10
+first_half = "full"
+second_half = "moon"
+complete = "full moon"
+complete = first_half + " " + second_half
+print('*`{0} {1} *'.format(first_half, second_half))
+print(complete)
