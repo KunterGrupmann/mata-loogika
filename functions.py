550a86eb309fe763b6b6ff5b7d0df4907e265fd7
Kunter <kunter.grupmann@voco.ee>
2022-11-09 13:13:31 +0200

    functions

---
+++ functions.py
@@ -0,0 +1,27 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    """print describe text."""
+    print("I´m inside the function")
+
+
+# my_name_is(name)
+def  my_name_is(name):
+    print("My name is " + name)
+    #print(f"My name is {name}")
+
+
+# sum_six(num)
+def sum_six(num):
+    return(6 + num)
+
+# sum_numbers()
+def sum_numbers(a, b):
+    return(a + b)
+
+
+# usd_to_eur()
+def usd_to_eur(dollar):
+    return(dollar * 0.8)
